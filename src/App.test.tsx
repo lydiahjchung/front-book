03a991a6b507e2716ebdbd5071dd93c4565e5e4e
Lydia <lydia@suppliesfitness.com>
2024-08-11 23:39:50 +0900

    fix: deploy storybook

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,9 +2,9 @@ import { describe, it, expect, test } from "vitest"
 import { render } from "@testing-library/react"
 import App from "./App"
 
-// test("demo", () => {
-//   expect(true).toBe(true)
-// })
+test("demo", () => {
+  expect(true).toBe(true)
+})
 
 describe("render", () => {
   it("renders the main page", () => {
